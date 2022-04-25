8edcc260d55f54e7d600d09772452ee07a5ea757
Yen Giang Nguyen <103374998+yengiang@users.noreply.github.com>
2022-04-26 01:45:44 +0700

    add question

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 # Given: A DNA string s of length at most 1000 nt. 
 # Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s.
 
+# Sao lại không and được ở khúc if mà phải tách ra làm 3 if riêng
+
 dna = 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
 
 #count A
