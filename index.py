1e641a1fdd3f2715f9b2d51dd5bbd326397eb863
Yen Giang Nguyen <yengiang.nguyen05@gmail.com>
2022-04-26 01:11:44 +0700

    add work 1

--- index.py
+++ index.py
@@ -1,1 +1,11 @@
-print("hello world")
+# Given: A DNA string s of length at most 1000 nt. 
+# Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s.
+
+dna = 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
+
+#count A
+for c in dna:
+    if c == 'G':
+        continue
+    print(c)
+
