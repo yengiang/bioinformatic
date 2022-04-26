392b7a27ed59286a9830aa128b747ffd343cc7df
Yen Giang Nguyen <yengiang.nguyen05@gmail.com>
2022-04-26 21:47:45 +0700

    solve 3

--- index.py
+++ index.py
@@ -1,14 +1,12 @@
 # Given: A DNA string s of length at most 1000 nt. 
 # Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s.
 
-# Sao lại không and được ở khúc if mà phải tách ra làm 3 if riêng
-
 dna = 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
 
 #count A
 lstA = []
 for c in dna:
-    if c == 'G' or c == 'T' or c == 'C':
+    if c != 'A':
         continue
     lstA.append(c)
 print(len(lstA))
@@ -16,7 +14,7 @@ print(len(lstA))
 #count G
 lstG = []
 for c in dna:
-    if c == 'A' or c == 'T' or c == 'C':
+    if c != 'G':
         continue
     lstG.append(c)
 print(len(lstG))
@@ -24,7 +22,7 @@ print(len(lstG))
 #count C
 lstC = []
 for c in dna:
-    if c == 'G' or c == 'T' or c == 'A':
+    if c != 'C':
         continue
     lstC.append(c)
 print(len(lstC))
@@ -32,7 +30,7 @@ print(len(lstC))
 #count T
 lstT = []
 for c in dna:
-    if c == 'G' or c == 'A' or c == 'C':
+    if c != 'T':
         continue
     lstT.append(c)
 print(len(lstT))
